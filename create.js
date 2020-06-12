16039141fb462dcabddb476247fc51c4033330fa
ltann <leeroyleeroy18@gmail.com>
2020-06-12 13:50:19 -0700

    Added API to return a list of all the notes a user has

--- create.js
+++ create.js
@@ -1,12 +1,9 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
@@ -26,32 +23,7 @@ export function main(event, context, callback) {
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
+  await dynamoDb.put(params);
 
-    // Return status code 500 on error
-    if (error) {
-      console.log(error);
-      console.log(AWS.config);
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
-}
+  return params.Item;
+});
